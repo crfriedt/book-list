a234fc21200f75beb9fb35c1c8bc3dc59544455d
Cory Friedt <c.friedt89@gmail.com>
2020-04-25 15:29:39 -0700

    added validation and alerts

--- app.js
+++ app.js
@@ -27,6 +27,27 @@ function UI(){}
     list.appendChild(row);
 }
 
+  // Show alert
+  UI.prototype.showAlert = function(message, className){
+  // Create div
+  const div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector('.container');
+  const form = document.querySelector('#book-form');
+  // Insert alert
+  container.insertBefore(div, form);
+
+  // Timeout after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').remove();
+  }, 3000);
+
+};
+
 // Clear fields
 UI.prototype.clearFields = function(){
   document.getElementById('title').value = '';
@@ -45,14 +66,21 @@ document.getElementById('book-form').addEventListener('submit', function(e){
   const book = new Book(title, author, isbn);
 
   // Instantiate UI
-
   const ui = new UI();
 
-  // Add book to list
-  ui.addBookToList(book);
+  // Validate
+  if(title === '' || author === '' || isbn === ''){
+    // Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
 
-  // Clear fields
-  ui.clearFields();
+    // Show success
+    ui.showAlert('Book Added!', 'success');
+    // Clear fields
+    ui.clearFields();
+  }
 
   e.preventDefault();
 });
